66591b444abc3d36bef00b199e659e95e0dbbc07
Raziel Kretschmer <kretschmer.raziel@gmail.com>
2022-05-03 15:14:43 +0200

    asfsdgfopskodgoi

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ rudolf.Init()
 rudolf.v = 2*2*math.pi*0.035
 
 
-def drive(speed: float, direction: float, time: float = 1):
+def drive(speed: float, direction: float = 0, time: float = 1):
     rudolf.SetMotorsEnabled(True)
     rudolf.SetMotor1(speed * (-1))
     rudolf.SetMotor2(speed)
-    time.sleep(time)
     rudolf.SetMotors(0)
     rudolf.SetServoPosition(direction)
 
 
-drive(1,1,3)
+drive(1,1)
--- pic.py
+++ pic.py
@@ -1,6 +1,9 @@
 import imageio.v3 as iio
 import numpy as np
 
+# TODO: Write own "downscaling" -> Take 4*4 pixels, make average color value (RGB) and and check against given color
+# Don't forget what happens when you're at the end of the pic
+
 img = iio.imread("test3.jpg")
 img_array = np.array(img)
 print(img_array[1000][100])
