d8304905b99ccdece51a0569eb453fe99ad5519e
Raziel Kretschmer <kretschmer.raziel@gmail.com>
2022-05-03 10:56:10 +0200

    prints...

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import RockyBorg
 import time
 
 rudolf = RockyBorg.RockyBorg()
+print("Init")
 rudolf.Init()
+print("Init done")
 rudolf.SetMotorsEnabled(True)
-print("Enabled Motots")
+print("Enabled Motors")
 rudolf.SetMotors(0.5)
 print("Set Motors 0,5")
 time.sleep(1)
-print("Set Motors 0")
+print("Seting Motors 0")
 rudolf.SetMotors(0)
+print("Set Motors 0")
