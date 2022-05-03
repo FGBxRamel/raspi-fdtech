577f81492a0805be56adacef51728a3326143e2f
Raziel Kretschmer <kretschmer.raziel@gmail.com>
2022-05-03 15:59:26 +0200

    work end

--- pic.py
+++ pic.py
@@ -1,8 +1,12 @@
 import imageio.v3 as iio
 import numpy as np
 
-# TODO: Write own "downscaling" -> Take 4*4 pixels, make average color value (RGB) and and check against given color
-# Don't forget what happens when you're at the end of the pic
+# TODO: Write search algo 
+# First: go trough every x pixel, look if pixel in color range, if yes then remember that pixel
+# Second: Go to every one of that pixel, make average of the color of x*x pixels around the pixel, save that
+# Third: Compare all the boxes, take one with highest match to given color
+# Fourth: World domination
+# PS: I know this is double linear, but idc
 
 img = iio.imread("test3.jpg")
 img_array = np.array(img)
