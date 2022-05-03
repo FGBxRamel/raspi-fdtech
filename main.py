434636af22c7cf8147c83e4d3ec5c63e25306f35
Raziel Kretschmer <kretschmer.raziel@gmail.com>
2022-05-03 12:11:52 +0200

    ojgf

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ rudolf.Init()
 
 rudolf.v = 2*2*math.pi*0.035
 
-def longt(richtung, speed : float):
+
+def longt(richtung, speed: float):
     rudolf.SetMotorsEnabled(True)
     rudolf.SetMotor1(0.5)
     rudolf.SetMotor2(0)
-    
-longt(1,0,5)
+
+
+longt(1, 0.5)
