84ef7e4a8661217fdd5c53e5cc57e60287fa8fe9
Raziel Kretschmer <kretschmer.raziel@gmail.com>
2022-05-03 12:24:51 +0200

    akf

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ rudolf.v = 2*2*math.pi*0.035
 
 def longt(richtung, speed: float):
     rudolf.SetMotorsEnabled(True)
-    rudolf.SetMotor1(-0.5)
-    rudolf.SetMotor2(0)
+    rudolf.SetMotor1(-1)
+    rudolf.SetMotor2(1)
+    input()
+    rudolf.SetMotors(0)
 
 
 longt(1, 0.5)
