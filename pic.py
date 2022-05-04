87a014b5229f29265d29fd0e8122cc8cc85acbf9
Raziel Kretschmer <kretschmer.raziel@gmail.com>
2022-05-04 16:31:50 +0200

    ailgfi

--- pic.py
+++ pic.py
@@ -1,42 +1,42 @@
-from turtle import width
-import imageio.v3 as iio
+from operator import length_hint
+import cv2 as cv
 import numpy as np
 
-# TODO: Write search algo
-# First: go trough every x pixel, look if pixel in color range, if yes then remember that pixel
-# Second: Go to every one of that pixel, make average of the color of x*x pixels around the pixel, save that
-# Third: Compare all the boxes, take one with highest match to given color
-# Fourth: World domination
 
-img = np.array(iio.imread("test3.jpg"))
-print(img.shape)
-length = 8
+class Farbe:
+    """A class for a color.\n
+    It needs a color code in the format of a list (R, G, B).\n
+    Optional arguments:\n
+        tolerance: is the range in which every color value ist okay (R+-x, G+-x, B+-x). Default: 5.\n
+        length: The length of the sides of the downscaled picture. The bigger the number, the more precise (and long) it will be. Default: 128
+    """
 
-start = list((0, 0))
-done = False
-fields = np.empty((int(img.shape[0]/length), int(img.shape[1]/length)), dtype=list)
-runx = 0
-runy = 0
-y = 0
+    def __init__(self, farbcode: list, tolerance: int = 5, length : int = 128) -> None:
+        self.farbe = list(farbcode)
+        self.tolerance = int(tolerance)
+        self.length = int(length)
+        self._addTolerance()
 
-while not done:
-    if img.shape[0] < y + length:
-        done = True
-    else:
-        median = list((0, 0, 0))
-        for y in range(start[0], start[0]+length):
-            for x in range(start[1], start[1]+length+1):
-                for i in range(0, 3):
-                    median[i] = median[i] + img[y, x][i]
-        for j in range(0, 3):
-            median[j] = median[j] / 64
-        
-        fields[runy, runx] = median
+    def _addTolerance(self) -> list:
+        """Internal function to add the given tolerance to the RGB list. Gives back a list, with a list with the start and stop value of the color value.\n
+        Example: list[0] -> (145, 155)\n
+        means that the R value can range from 145 to 155. list[1]; list[2] is G; B.
+        """
+        self.range = []
+        for i in range(0, 3):
+            self.range[i] = list(
+                (self.farbe[i]-self.tolerance, self.farbe[i]+self.tolerance))
+        return self.range
 
-        if img.shape[1] < start[1] + length:
-            start[0], start[1] = start[0] + length, 0
-            runy += 1
-        else:
-            start[1] = start[1] + length
-            runx += 1
-print(fields)
+    def getY(self):
+        # TODO Check if the found field is in an acceptable color range
+        # If not: return None
+        # TODO Change image to freshly taken one
+        img = np.array(cv.resize(cv.imread("test3.jpg"), (self.length, self.length)))
+        for y in range(0, self.length):
+            for x in range(0, self.length):
+                for i, pair in enumerate(self.range):
+                    if img[y, x][i] <= pair[0] or img[y, x][i] >= pair[1]:
+                        abs()
+
+
