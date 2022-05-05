dd7435d04e90df74a5021d5ce2b3e3083cbd5bf4
Raziel Kretschmer <kretschmer.raziel@gmail.com>
2022-05-05 08:54:27 +0200

    Yay

--- pic.py
+++ pic.py
@@ -12,7 +12,7 @@ class Color:
         length: The length of the sides of the downscaled picture. The bigger the number, the more precise (and long) it will be. Default: 128
     """
 
-    def __init__(self, farbcode: list, tolerance: int = 20, length: int = 128) -> None:
+    def __init__(self, farbcode: list[int], tolerance: int = 20, length: int = 128) -> None:
         self.color = list(farbcode)
         self.tolerance = int(tolerance)
         self.length = int(length)
@@ -32,27 +32,34 @@ class Color:
 
     def getY(self) -> int:
         """Gives back the y-Coordinate of the last pixel found matching the color."""
-        # TODO Change image to freshly taken one... Idealy from stream
-        # TODO This always gives back a number??
         with self.camera.capture(io.BytesIO(), 'jpeg') as f:
             img = np.array(cv.resize(cv.imread(f),
                                      (self.length, self.length)))
-        #img = np.array(cv.resize(cv.imread("test3.jpg"),
+        # img = np.array(cv.resize(cv.imread("test3.jpg"),
         #               (self.length, self.length)))
-        winner = {}
         # this dict has the structure: avg_offset : pixel-y-coordinate
+        winner = {}
         for y in range(0, self.length):
             for x in range(0, self.length):
+                # avg_offset is calculated with abs(pixel R_Value - Color_R_Value) + abs(G_value - Color_G_Value) + abs(B_Value - Color_B_Value)
+                # It should find the pixel which is nearest to the given color
                 avg_offset = 0
+                # Get pairs of the color values, as seen in the _addTolerance func, and the index of them
                 for i, pair in enumerate(self.range):
+                    # Get the R/G/B Value of the pixel
                     img_color = img[y, x][i]
+                    # If the pixel color is in the range, add the offset to the average, if not just ignore pixel
                     if img_color >= pair[0] and img_color <= pair[1]:
                         avg_offset += abs(img_color - self.color[i])
+                        valid = True
                     else:
+                        valid = False
                         break
-                winner[avg_offset] = y
+                if valid:
+                    winner[avg_offset] = y
         return winner[min(winner.keys())] if not len(winner) == 0 else None
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     color = Color((212, 25, 196), tolerance=0)
     print(color.getY())
