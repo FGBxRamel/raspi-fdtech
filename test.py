d22a232b0897f4f1ff2aafe55537f3c8a55140fa
Karaki-kun <100281208+Karaki-kun@users.noreply.github.com>
2022-05-05 15:44:36 +0200

    123

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import ultrasonic_sensor
 stopdist = 10
 
 #returns distance in cm's
-dist = ultrasonic_sensor.distance
+dist = ultrasonic_sensor.distance()
 print(dist)
 """#stopps if distance is to low
 if dist < stopdist:
