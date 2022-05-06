f535d5c1919f984bf3f8e10a158a53cfbb27ae66
Karaki-kun <100281208+Karaki-kun@users.noreply.github.com>
2022-05-06 10:31:56 +0200

    123

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import ultrasonic_sensor
 import time
+import math
 stopdist = 10
 n = 0
 while n < 5:
-    print(30/ultrasonic_sensor.distance())
+    print(math.asin(30/ultrasonic_sensor.distance()))
     time.sleep(1)
     n += 1
 
